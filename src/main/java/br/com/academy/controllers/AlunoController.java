870399966b7cd77a8bd4085d51a21fcbf65bfc5b
Paulo Vilela <paulov4@hotmail.com>
2025-07-17 22:58:37 -0300

    Criando método Editar e Excluir Aluno

--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -5,6 +5,7 @@ import br.com.academy.model.Aluno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -39,5 +40,30 @@ public class AlunoController {
         mv.addObject("alunosList", alunorepositorio.findAll());
         return mv;
     }
+
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Integer id){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("Aluno/alterar");
+        Aluno aluno = alunorepositorio.getOne(id);
+        mv.addObject("aluno", aluno);
+        return mv;
+    }
+
+    @PostMapping("/alterar")
+    public ModelAndView alterar(Aluno aluno){
+        ModelAndView mv = new ModelAndView();
+        alunorepositorio.save(aluno);
+        mv.setViewName("redirect:/alunos-adicionados");
+        return  mv;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirAluno(@PathVariable("id") Integer id){
+        alunorepositorio.deleteById(id);
+        return "redirect:/alunos-adicionados";
+    }
+
+
 
 }
